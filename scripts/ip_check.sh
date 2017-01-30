c582c4d5a8ff5a686c07c45e8e43e19d8e0c337d
Szubie <benszubert@gmail.com>
2017-01-30 02:02:05 +0000

    Switch to using absolute path to load config file in ip_check.sh, enable cron to find the file

--- scripts/ip_check.sh
+++ scripts/ip_check.sh
@@ -8,7 +8,7 @@ inet=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && 
 
 if [[ $inet == 'Online' ]]; then echo $inet;
     if [[ $old_ip != $current_ip ]]; then
-        readarray -t recipients < config/email/emailDestination.config
+        readarray -t recipients < /opt/ipEmailer/config/email/emailDestination.config
         for recipient in ${recipients[@]}; do
             echo "$(sed "s/CURRENT_IP/$current_ip/g" ~/.ipEmailer/config/email/emailText.txt)" | mail -s "$(sed "s/CURRENT_IP/$current_ip/g" ~/.ipEmailer/config/email/emailTitle.txt)" $recipient
         done
